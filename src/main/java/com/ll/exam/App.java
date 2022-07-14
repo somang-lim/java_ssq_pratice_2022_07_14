59398f56cf850344d0905998bb4b7aca8e6eda04
somang-lim <somangjuha@gmail.com>
2022-07-14 16:48:46 +0900

    등록시 생성된 명언번호 노출

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -19,6 +19,13 @@ public class App {
             String cmd = sc.nextLine().trim();
 
             switch(cmd) {
+                case "등록" :
+                    System.out.print("명언 : ");
+                    String content = sc.nextLine();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine();
+                    System.out.println("1번 명언이 등록되었습니다.");
+                    break;
                 case "종료" :
                     break outer;
             }
