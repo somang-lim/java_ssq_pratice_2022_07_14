c975de1f59efae218f22c1f682552f51f6531be4
somang-lim <somangjuha@gmail.com>
2022-07-14 15:19:03 +0900

    세팅

---
+++ src/main/java/com/ll/exam/Main.java
@@ -0,0 +1,9 @@
+package com.ll.exam;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,16 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void 테스트() {
+        assertTrue(1 == 1);
+        assertEquals(1, 1);
+    }
+
+}
